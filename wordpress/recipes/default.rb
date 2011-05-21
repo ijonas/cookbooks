6fcc801b3bdb579e7130ecdf364d1698a3ab9f40
Ijonas Kisselbach <ijonas.kisselbach@gmail.com>
2011-05-21 17:09:54 +0100

    new wordpress

--- wordpress/recipes/default.rb
+++ wordpress/recipes/default.rb
@@ -44,13 +44,6 @@ execute "untar-wordpress" do
   creates "#{node[:wordpress][:dir]}/wp-settings.php"
 end
 
-execute "move-wordpress-into-root" do
-  cwd node[:wordpress][:dir]
-  command "mv wordpress/* ."
-  creates "#{node[:wordpress][:dir]}/index.php"
-  action :run
-end
-
 execute "mysql-install-wp-privileges" do
   command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} < /etc/mysql/wp-grants.sql"
   action :nothing
@@ -84,12 +77,6 @@ execute "create #{node[:wordpress][:db][:database]} database" do
 end
 
 # save node data after writing the MYSQL root password, so that a failed chef-client run that gets this far doesn't cause an unknown password to get applied to the box without being saved in the node data.
-ruby_block "save node data" do
-  block do
-    node.save
-  end
-  action :create
-end
 
 log "Navigate to 'http://#{server_fqdn}/wp-admin/install.php' to complete wordpress installation" do
   action :nothing
